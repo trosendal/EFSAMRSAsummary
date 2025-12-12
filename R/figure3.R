84f65123b0e7ec391a63e3fcba8a39d3b8657ba9
Thomas Rosendal <thomas.rosendal@sva.se>
2025-12-12 15:25:23 +0100

    bug

--- R/figure3.R
+++ R/figure3.R
@@ -7,9 +7,9 @@
 figure3 <- function() {
     df <- read_AMR()
     setDT(df)
-    sum(df[!is.na(CC_infer),
-           .N/19,
-           .(animalclass,
-             host_association,
-             CC_infer %in% c("398", "CC1/CC398"))]$V1)
+    df[!is.na(CC_infer),
+       .N/19,
+       .(animalclass,
+         host_association,
+         CC_infer %in% c("398", "CC1/CC398"))]
 }
